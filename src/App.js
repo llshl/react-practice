ebc6344faba7a890cc1fc43f91b6d91975d0263a
llshl <lsh970708@naver.com>
2022-05-30 02:34:38 +0900

    반복문으로 div 줄이기

--- src/App.js
+++ src/App.js
@@ -4,35 +4,69 @@ import { useState } from 'react';
 
 function App() {
   let post = '강남 우동 맛집2';
-  let [글제목, b] = useState(['남자 코트 추천', '여자 코트 추천', '기타']);
-  let [따봉, 따봉변경] = useState(0);
+  let [titles, setTitle] = useState(['남자 코트 추천', '여자 코트 추천', '기타']);
+  let likeList = [];
+  titles.map(() => {
+    likeList.push(0);
+  });
+  let [like, setLike] = useState(likeList);
+  let [modal, setModal] = useState(false);
 
   return (
     <div className="App">
       <div className="black-nav">
         <h4>블로그임ㅋㅋ</h4>
       </div>
-      <div className="list">
-        <h4>
-          {글제목[0]}{' '}
-          <span
-            onClick={() => {
-              따봉변경(따봉 + 1);
-            }}
-          >
-            👍
-          </span>{' '}
-          {따봉}
-        </h4>
-        <p>5월 27일 발행</p>
-      </div>
-      <div className="list">
-        <h4>{글제목[1]}</h4>
-        <p>5월 27일 발행</p>
-      </div>
-      <Post/>
+      {/*<div className="list">*/}
+      {/*  <h4>*/}
+      {/*    {글제목[0]}{' '}*/}
+      {/*    <span*/}
+      {/*      onClick={() => {*/}
+      {/*        따봉변경(따봉 + 1);*/}
+      {/*      }}*/}
+      {/*    >*/}
+      {/*      👍*/}
+      {/*    </span>{' '}*/}
+      {/*    {따봉}*/}
+      {/*  </h4>*/}
+      {/*  <p>5월 27일 발행</p>*/}
+      {/*</div>*/}
+      {/*<div className="list">*/}
+      {/*  <h4>{글제목[1]}</h4>*/}
+      {/*  <p>5월 27일 발행</p>*/}
+      {/*</div>*/}
+      {/*<div className="list">*/}
+      {/*  <h4*/}
+      {/*    onClick={() => {*/}
+      {/*      setModal(!modal);*/}
+      {/*    }}*/}
+      {/*  >*/}
+      {/*    {글제목[2]}*/}
+      {/*  </h4>*/}
+      {/*  <p>5월 27일 발행</p>*/}
+      {/*</div>*/}
+      {/*{modal ? <Modal /> : null}*/}
 
-      <Modal />
+      {titles.map((post, index) => {
+        return (
+          <div className="list" key={index}>
+            <h4>
+              {post}
+              <span
+                onClick={() => {
+                  let copy = [...like];
+                  copy[index] += 1;
+                  setLike(copy);
+                }}
+              >
+                👍
+              </span>
+              {like[index]}
+            </h4>
+            <p>5월 27일 발행</p>
+          </div>
+        );
+      })}
     </div>
   );
 }
@@ -45,15 +79,6 @@ const Modal = () => {
       <p>상세내용</p>
     </div>
   );
-}
-
-const Post = () => {
-  return (
-    <div className="list">
-      <h4>기타</h4>
-      <p>5월 27일 발행</p>
-    </div>
-  );
-}
+};
 
 export default App;
