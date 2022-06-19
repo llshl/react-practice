c11059742a56add8cb77e4d9047ad04562f9940f
llshl <lsh970708@naver.com>
2022-06-19 18:47:09 +0900

    글 추가, 삭제기능

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
   let [like, setLike] = useState(likeList);
   let [modal, setModal] = useState(false);
   let [modelTitle, setModelTitle] = useState(0);
+  let [input, setInput] = useState('');
   return (
     <div className="App">
       <div className="black-nav">
@@ -28,7 +29,8 @@ function App() {
             >
               {post}
               <span
-                onClick={() => {
+                onClick={e => {
+                  e.stopPropagation(); // 이벤트 버블링 방지
                   let copy = [...like];
                   copy[index] += 1;
                   setLike(copy);
@@ -39,9 +41,45 @@ function App() {
               {like[index]}
             </h4>
             <p>5월 27일 발행</p>
+            <button
+              onClick={() => {
+                let copy = [...titles];
+                copy.splice(index, 1); // 배열 중간에서 삭제하기, index번째부터 1개 삭제
+                setTitle(copy);
+              }}
+            >
+              삭제
+            </button>
           </div>
         );
       })}
+
+      {/*<input*/}
+      {/*  onChange={e => {*/}
+      {/*    setInput(e.target.value); // 비동기함수라 최초 1개 입력했을땐 아무것도 나오지 않고 2개부터 1개씩 차이나게 나, 즉 이거 완료되기 전에*/}
+      {/*    console.log(input); // 이거부터 처리됨*/}
+      {/*  }}*/}
+      {/*/>*/}
+
+      {/* 글 새로 발행하기 */}
+      {/* input태그로 받아서 input state에 넣어줌 */}
+      <input
+        onChange={e => {
+          setInput(e.target.value); // 비동기함수라 최초 1개 입력했을땐 아무것도 나오지 않고 2개부터 1개씩 차이나게 나, 즉 이거 완료되기 전에
+          console.log(input); // 이거부터 처리됨
+        }}
+      />
+
+      {/* input state를 글목록 state에 넣어줌 */}
+      <button
+        onClick={() => {
+          let copy = [...titles];
+          copy.unshift(input);
+          setTitle(copy);
+        }}
+      >
+        글발행
+      </button>
       {modal ? <Modal setTitle={setTitle} titles={titles} modelTitle={modelTitle} /> : null}
     </div>
   );
